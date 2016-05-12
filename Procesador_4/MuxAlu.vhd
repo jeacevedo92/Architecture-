8d0924144a013d59a5aa6dae8dcb26c1b23d08e5
jeacevedo92 <jeacevedo92@utp.edu.co>
2016-05-12 17:29:25 -0500

    se agregan mux

---
+++ Procesador_4/MuxAlu.vhd
@@ -0,0 +1,39 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MuxAlu is
+    Port ( crs2 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  i : in  STD_LOGIC;
+           sing_ext : in  STD_LOGIC_VECTOR (31 downto 0);
+           op2 : out  STD_LOGIC_VECTOR (31 downto 0));
+end MuxAlu;
+
+architecture Behavioral of MuxAlu is
+
+begin
+process(crs2 , i , sing_ext)
+	begin
+   if (i = '0') then
+      op2 <= crs2;
+   else
+		op2 <= sing_ext; 
+   end if;
+
+	
+	
+end process;
+
+
+
+end Behavioral;
+
---
+++ Procesador_4/MuxNpc.vhd
@@ -0,0 +1,41 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MuxNpc is
+    Port ( PcSource : in  STD_LOGIC_VECTOR (1 downto 0);
+           Pcdisp30 : in  STD_LOGIC_VECTOR (31 downto 0);
+           Pcdisp22 : in  STD_LOGIC_VECTOR (31 downto 0);
+           Pc : in  STD_LOGIC_VECTOR (31 downto 0);
+           AluOut : in  STD_LOGIC_VECTOR (31 downto 0);
+           toNpc : out  STD_LOGIC_VECTOR (31 downto 0));
+end MuxNpc;
+
+architecture Behavioral of MuxNpc is
+
+begin
+process(Pcsource,Pcdisp30,Pcdisp22,Pc,AluOut)
+	begin 
+	case (pcSource) is 
+      when "00" =>
+         toNpc <= Pcdisp30 ;
+      when "01" =>
+         toNpc <= Pcdisp22 ;
+      when "10" =>
+         toNpc <= Pc ;
+      when "11" =>
+         toNpc <= AluOut;
+   end case;
+
+end process;
+
+end Behavioral;
+
